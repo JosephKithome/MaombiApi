cecd586a1c5278aad170b695ad75b89ea286b11f
Joseph Kithome <95905779+jmulingwa@users.noreply.github.com>
2022-06-21 12:56:38 +0300

    added bash scripts to automate build and deploy

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -3,5 +3,6 @@ chmod +x /home/ec2-user/server/* jar
 chmod +x /home/ec2-user/server/server_start.sh
 chmod +x /home/ec2-user/server/server_stop.sh
 
-chmod +x gradlew
+chmod +x gradlew && ./gradlew compileDebug --stacktrace
+
 
